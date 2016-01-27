ca1037bfe42f80b1d4a78b117af5062d96cf5349
QuickSander <sander@vanwoensel.me>
2016-01-27 23:26:24 +0100

    Renamed .h to .hpp

---
+++ src/StreamHttpReply.hpp
@@ -0,0 +1,76 @@
+//
+//  StreamHttpReply.h
+//  ArduinoHttpServer
+//
+//  Created by Sander van Woensel on 23-01-16.
+//  Copyright (c) 2016 Sander van Woensel. All rights reserved.
+//
+
+#ifndef __ArduinoHttpServer__StreamHttpReply__
+#define __ArduinoHttpServer__StreamHttpReply__
+
+#include <Arduino.h>
+
+namespace ArduinoHttpServer
+{
+
+//------------------------------------------------------------------------------
+//                             Class Declaration
+//------------------------------------------------------------------------------
+//! Shared Reply functionality
+class AbstractStreamHttpReply
+{
+
+public:
+    virtual void send(const String& data, const String& title);
+
+protected:
+   AbstractStreamHttpReply(Stream& stream, const String& contentType, const String& code);
+   virtual Stream& getStream();
+   virtual const String& getCode();
+   virtual const String& getContentType();
+
+private:
+   static const String DEFAULT_CONTENT_TYPE;
+
+   Stream& m_stream;
+   String m_contentType; //!< Needs to be overridden to default when required. Therefore not const.
+   const String m_code;
+
+};
+
+//------------------------------------------------------------------------------
+//                             Class Declaration
+//------------------------------------------------------------------------------
+//! Error reply
+class StreamHttpErrorReply: public AbstractStreamHttpReply
+{
+public:
+    StreamHttpErrorReply(Stream& stream, const String& contentType, const String& code = "400");
+    virtual void send(const String& data);
+
+protected:
+    virtual String getHtmlBody(const String& data);
+    virtual String getJsonBody(const String& data);
+};
+
+//------------------------------------------------------------------------------
+//                             Class Declaration
+//------------------------------------------------------------------------------
+//! Normal reply.
+class StreamHttpReply: public AbstractStreamHttpReply
+{
+public:
+    StreamHttpReply(Stream& stream, const String& contentType);
+
+    virtual void send(const String& data) { AbstractStreamHttpReply::send(data, "OK"); };
+
+
+protected:
+
+};
+
+
+}
+
+#endif // __ArduinoHttpServer__StreamHttpReply__
