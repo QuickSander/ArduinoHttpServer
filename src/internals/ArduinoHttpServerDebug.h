0d927dbbbff103dc4e008244b04497c20cd6527c
BF <bf@tbwb.nl>
2022-11-19 15:26:28 +0100

    Add OPTIONS method

--- src/internals/ArduinoHttpServerDebug.h
+++ src/internals/ArduinoHttpServerDebug.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2016 Sander van Woensel. All rights reserved.
 //
 //! Debug support
-
 #ifdef ARDUINO_HTTP_SERVER_DEBUG
    #define DEBUG_ARDUINO_HTTP_SERVER_PRINT(...) Serial.print(__VA_ARGS__)
    #define DEBUG_ARDUINO_HTTP_SERVER_PRINTLN(...) Serial.println(__VA_ARGS__)
